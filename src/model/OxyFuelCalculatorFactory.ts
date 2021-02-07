2390f8345dffff076653a46c4cd80df690dd7372
alexesprit <alex.esprit@gmail.com>
2021-02-07 11:09:09 +0300

    Add calculator for oxy-acetylene cutting

--- src/model/OxyFuelCalculatorFactory.ts
+++ src/model/OxyFuelCalculatorFactory.ts
@@ -3,14 +3,18 @@ import { OxyFuelCalculator } from '@/model/OxyFuelCalculator';
 import { FuelType } from '@/model/FuelType';
 
 import { oxyPropaneFactors } from '@/model/OxyPropaneFactors';
+import { oxyAcetyleneFactors } from '@/model/OxyAcetyleneFactors';
 
 export function createOxyFuelCalculator(fuelType: FuelType): OxyFuelCalculator {
 	switch (fuelType) {
+		case FuelType.Acetylene:
+			return new BaseOxyFuelCalculator(oxyAcetyleneFactors);
+
 		case FuelType.Propane:
 			return new BaseOxyFuelCalculator(oxyPropaneFactors);
 	}
 }
 
 export function getAvailableFuels(): FuelType[] {
-	return [FuelType.Propane];
+	return [FuelType.Acetylene, FuelType.Propane];
 }
