54c48f0d3d1efa9157efc268f08381bbc6348766
alexesprit <alex.esprit@gmail.com>
2021-02-07 10:55:54 +0300

    Merge factor maps

--- src/model/BaseOxyFuelCalculator.ts
+++ src/model/BaseOxyFuelCalculator.ts
@@ -1,37 +1,34 @@
 import { OxyFuelCalculator } from '@/model/OxyFuelCalculator';
 import { OxyFuelUsage } from '@/model/OxyFuelUsage';
 
-export type FactorMap = Record<number, number>;
+/**
+ * [Oxygen, Fuel] usage per one meter.
+ */
+export type OxyFuelFactor = [number, number];
+export type OxyFuelFactorMap = Record<number, OxyFuelFactor>;
 
 export class BaseOxyFuelCalculator implements OxyFuelCalculator {
-	constructor(
-		private oxygenFactors: FactorMap,
-		private fuelFactors: FactorMap
-	) {}
+	constructor(private oxyFuelFactors: OxyFuelFactorMap) {}
 
 	calculateUsage(thickness: number, cutLength: number): OxyFuelUsage {
-		if (!(thickness in this.oxygenFactors)) {
+		if (!(thickness in this.oxyFuelFactors)) {
 			throw new Error(`Unsupported thickness: ${thickness}`);
 		}
 
-		if (!(thickness in this.fuelFactors)) {
-			throw new Error(`Unsupported thickness: ${thickness}`);
-		}
+		const [oxygenFactor, fuelFactor] = this.oxyFuelFactors[thickness];
 
-		const oxygenFactor =
-			this.oxygenFactors[thickness] *
-			oxygenConversionFactor *
-			startCuttingFactor;
-		const fuelFactor = this.fuelFactors[thickness] * startCuttingFactor;
+		const oxygenUsagePerMeter =
+			oxygenFactor * oxygenConversionFactor * startCuttingFactor;
+		const fuelUsagePerMeter = fuelFactor * startCuttingFactor;
 
-		const oxygen = (cutLength * oxygenFactor) / 1000;
-		const fuel = (cutLength * fuelFactor) / 1000;
+		const oxygen = (cutLength * oxygenUsagePerMeter) / 1000;
+		const fuel = (cutLength * fuelUsagePerMeter) / 1000;
 
 		return { oxygen, fuel };
 	}
 
 	getThicknessSupported(): readonly number[] {
-		return Object.keys(this.oxygenFactors).map((key) =>
+		return Object.keys(this.oxyFuelFactors).map((key) =>
 			Number.parseInt(key)
 		);
 	}
--- src/model/OxyFuelCalculatorFactory.ts
+++ src/model/OxyFuelCalculatorFactory.ts
@@ -1,18 +1,13 @@
 import { BaseOxyFuelCalculator } from '@/model/BaseOxyFuelCalculator';
 import { OxyFuelCalculator } from '@/model/OxyFuelCalculator';
 import { FuelType } from '@/model/FuelType';
-import {
-	oxyPropaneOxygenFactors,
-	oxyPropanePropaneFactors,
-} from '@/model/OxyPropaneFactors';
+
+import { oxyPropaneFactors } from '@/model/OxyPropaneFactors';
 
 export function createOxyFuelCalculator(fuelType: FuelType): OxyFuelCalculator {
 	switch (fuelType) {
 		case FuelType.Propane:
-			return new BaseOxyFuelCalculator(
-				oxyPropaneOxygenFactors,
-				oxyPropanePropaneFactors
-			);
+			return new BaseOxyFuelCalculator(oxyPropaneFactors);
 	}
 }
 
